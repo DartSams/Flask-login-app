f87effa9885c7c5b71e1c4b08e1978455ad8d1b6
DartSams <dartagnansams1@icloud.com>
2021-05-06 21:52:24 -0400

    added hash password ability

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template,request,redirect
+# from flask.ext.bcrypt import Bcrypt
 from flask_bcrypt import Bcrypt
 
 
-##docs to encrypt passwd https://flask-bcrypt.readthedocs.io/en/latest/
+##to encrypt passwd https://flask-bcrypt.readthedocs.io/en/latest/
 app=Flask(__name__)
 bcrypt=Bcrypt(app)
-
+login_state=False
 
 @app.route('/')
 def home():
@@ -14,35 +15,55 @@ def home():
 
 @app.route('/login' ,methods=["GET","POST"])
 def login():
+    global login_state
     if request.method=="POST":
         username = request.form['username']
         password = request.form['password']
-        hash_passwd = bcrypt.generate_password_hash(password).decode('utf-8')
-
-        # print(username,password)
-        print(f"\ndictionary of username + password: {request.form}")
-        print(f"hashed password: {hash_passwd}\n")
-
-
-        ##check if database passwd and entered password are the same
-        candidate = 'secret'
-        passwd_check=bcrypt.check_password_hash(hash_passwd, candidate)
-        print(passwd_check)
-
 
         if username and password != "":
-                if passwd_check==True:
-                    return redirect(f'/profile/{username}')
+            hash_passwd = bcrypt.generate_password_hash(password).decode('utf-8')
+            example = 'a'
+            passwd_check=bcrypt.check_password_hash(hash_passwd, example)
+            print(passwd_check)
+            if passwd_check == True:
+                login_state=True
+                return redirect(f'/profile/{username}')
+            
+            elif passwd_check == False:
+                return redirect('/login')
+
+        elif username and password == "" or password == "":
+            print('You must fill in the username and password fields')
+            return redirect('/login')
 
 
-    if request.method=="GET":
+        # print(username,password)
+        # print(f"\ndictionary of username + password: {request.form}")
+        # print(f"hashed password: {hash_passwd}\n")
+
+
+        # ##check if database passwd and entered password are the same
+        # example = 'secret'
+        # passwd_check=bcrypt.check_password_hash(hash_passwd, example)
+        # print(passwd_check)
+
+
+    elif request.method=="GET":
         username=""
         password=""
         return render_template('login.html')
 
 @app.route('/profile/<name>')
 def profile(name):
-    return render_template('profile.html',name=name)
+    global login_state
+
+    if login_state==False:
+        return redirect('/login')
+
+    else:
+        login_state=False
+        return render_template('profile.html',name=name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
